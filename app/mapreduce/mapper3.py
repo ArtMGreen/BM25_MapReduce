371b6ac8d0ca2b5ccb997a0f58599506dd6ba3b6
ArtMGreen <57835642+ArtMGreen@users.noreply.github.com>
2025-04-16 00:00:06 +0300

    Add files via upload

---
+++ app/mapreduce/mapper3.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import sys
+
+
+for line in sys.stdin:
+    line = line.strip()
+    try:
+        document_id, title, text = line.split("\t", maxsplit=2)
+        document_id = int(document_id)
+        for word in text.split():
+            print(f'{document_id}\t{title}\t1')
+    except:
+        continue
